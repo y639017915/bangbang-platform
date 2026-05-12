1280405ebe82aee92b5ee2ede1e403bdda80ef6a
y639017915 <y639017915@github.com>
2026-05-12 09:12:23 +0000

    Fix main.tsx syntax error

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,8 +5,7 @@ import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  &lt;React.StrictMode&gt;
-    &lt;App /&gt;
-  &lt;/React.StrictMode&gt;,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
-
